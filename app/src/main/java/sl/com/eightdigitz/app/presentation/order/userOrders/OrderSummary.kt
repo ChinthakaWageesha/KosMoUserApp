98d591feed3a5b07137da218d615655dcd787ce4
Wageesha Chinthaka <chinthaka.projects@gmail.com>
2021-03-01 23:52:20 +0530

    order summary UI implemented with adapters

--- app/src/main/java/sl/com/eightdigitz/app/presentation/order/userOrders/OrderSummary.kt
+++ app/src/main/java/sl/com/eightdigitz/app/presentation/order/userOrders/OrderSummary.kt
@@ -1,24 +1,22 @@
 package sl.com.eightdigitz.app.presentation.order.userOrders
 
 import android.os.Bundle
-import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_order_to_proceed.*
+import kotlinx.android.synthetic.main.activity_order_summary.*
 import sl.com.eightdigitz.app.R
 import sl.com.eightdigitz.app.presentation.order.userOrders.adapters.OrderStagesAdapter
 import sl.com.eightdigitz.core.base.BaseActivity
 import sl.com.eightdigitz.presentation.Constant
 import sl.com.eightdigitz.presentation.extensions.setAppActionBar
 import sl.com.eightdigitz.presentation.extensions.setRoundedImage
-import sl.com.eightdigitz.presentation.extensions.startActivity
 
-class OrderToProceed : BaseActivity(), View.OnClickListener {
+class OrderSummary : BaseActivity() {
 
     private var orderStageList = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order_to_proceed)
+        setContentView(R.layout.activity_order_summary)
         setToolbar()
         init()
     }
@@ -33,45 +31,38 @@ class OrderToProceed : BaseActivity(), View.OnClickListener {
 
     private fun init() {
         setData()
-        setOrderStageAdapter()
-        btn_decline_proceed_order.setOnClickListener(this)
-        btn_proceed_order.setOnClickListener(this)
+        setOrderSummaryAdapter()
     }
 
     private fun setData() {
-        iv_proceed_order_talent_image.setRoundedImage(
+        iv_order_summary_talent_image.setRoundedImage(
             url = Constant.USER_IMAGE_AQUAMAN,
             radius = 12
         )
-        tv_proceed_order_talent_name.text = "Norman Munoz"
-        tv_proceed_order_talent_field.text = "Actress"
-        tv_proceed_order_reference.text = "Order Reference RTX123HJ"
-        tv_proceed_order_due_date.text = "Due by 16 Mar 2021, 08:08 PM"
-        tv_proceed_order_for_username.text = "Nadia Ismail"
+        tv_order_summary_talent_name.text = "Norman Munoz"
+        tv_order_summary_talent_field.text = "Actress"
+        tv_order_summary_reference.text = "Order Reference RTX123HJ"
+        tv_order_summary_due_date.text = "Due by 16 Mar 2021, 08:08 PM"
+        tv_order_summary_for_username.text = "Nadia Ismail"
         tv_address_as.text = "Address Nadia Ismail as"
-        tv_address_proceed_order_user_as.text = "She/Her"
-        tv_proceed_order_message.text = "Roast Message"
-        tv_proceed_order_remaining_hrs.text = "You have 12 hours remaining to review the order status. \n" + "By default the order will proceed."
+        tv_address_order_summary_user_as.text = "She/Her"
+        tv_order_summary_message.text = "Roast Message"
     }
 
-    private fun setOrderStageAdapter() {
+    private fun setOrderSummaryAdapter() {
         orderStageList.add(0, "Reviewing")
         orderStageList.add(1, "Processing")
         orderStageList.add(2, "On Hold")
-        rv_proceed_order_stages.adapter = OrderStagesAdapter(orderStageList)
-        rv_proceed_order_stages.layoutManager =
+        orderStageList.add(3, "Approved")
+        orderStageList.add(4, "Expired")
+        orderStageList.add(5, "Refund")
+        rv_order_summary_stages.adapter = OrderStagesAdapter(orderStageList)
+        rv_order_summary_stages.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         goBack()
         return super.onSupportNavigateUp()
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btn_decline_proceed_order -> goBack()
-            R.id.btn_proceed_order -> startActivity<OrderSummary>()
-        }
     }
 }
